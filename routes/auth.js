1021476a083eeab28205c79e5595950658092212
Mutairu-Lawal <thebillionnairadream@gmail.com>
2026-01-17 15:24:05 +0000

    done with register endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcrypt');
+const fs = require('fs/promises');
 
 const router = express.Router();
 // route for registering user
@@ -16,14 +17,28 @@ router.post(
       if (!error.isEmpty()) throw error;
       const { password, email, role, name } = req.body;
       const hashedPassword = await bcrypt.hash(password, 10);
+
+      // laod db
+      const db = await fs.readFile('dist/users.json', 'utf-8');
+      const jsonDB = JSON.parse(db);
+
+      // constructing the ID
+      let lastIndexID = jsonDB.usersDB.at(-1)?.id;
+
       const newUser = {
-        id: 1,
+        id: lastIndexID ? (lastIndexID += 1) : 1,
         name,
         email,
         role: role ? role.trim() : 'member',
         password: hashedPassword,
       };
-      res.status(201).json(newUser);
+
+      // store into the db
+      const updatedDB = { ...jsonDB, usersDB: [...jsonDB.usersDB, newUser] };
+
+      // await fs.writeFile('dist/users.json', JSON.stringify(updatedDB));
+
+      res.status(201).json({ message: 'User created' });
     } catch (error) {
       res.status(400).json(error);
     }
